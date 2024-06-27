ed9dd0f72908c1b768c62133a3774aebca205bf3
Sandhiya-Ajithkumar <sandhiyashankar24@gmail.com>
2024-06-27 14:17:08 +0530

    commit

---
+++ src/test/java/day/sandy.java
@@ -0,0 +1,16 @@
+package day;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class sandy {
+
+	@Test
+	public void testng() {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.youtube.com");
+		
+
+	}
+
+}
